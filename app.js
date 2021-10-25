3dd28df33050d38c9de64f7c483b5e4b8fa6be0e
Mishra swapnil <sammishra644@gmail.com>
2021-10-25 22:29:55 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+/** to show the script is working 
+ console.log("script is working from different file")**/
+// using prompt for input
+var username = prompt("Give me username")
+
+//using promt for processing
+var welcomeMessage = "this script works!! " + username;
+
+//output
+alert(welcomeMessage);
