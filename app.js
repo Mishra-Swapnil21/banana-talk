e6ddb5b083e033e4ff20bb2c59edad978945afdd
Mishra swapnil <sammishra644@gmail.com>
2021-10-25 23:16:47 +0530

    taking input in console

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /** to show the script is working 
  console.log("script is working from different file")**/
+/**
+ 
 // using prompt for input
 var username = prompt("Give me username")
 
@@ -8,3 +10,28 @@ var welcomeMessage = "this script works!! " + username;
 
 //output
 alert(welcomeMessage);
+
+**/
+
+// button
+
+var btnTranslate =  document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input"); 
+// show it in console 
+//console.log(btnTranslate)
+console.log(txtInput)
+//event listener
+/**btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked")
+})**/
+function clickHandler(){
+    console.log("clicked!");
+    console.log("Input:-", txtInput.value);
+};
+
+btnTranslate.addEventListener("click", clickHandler)
+
+
+
+
+
